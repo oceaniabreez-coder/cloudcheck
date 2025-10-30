7769b1de99197eb725707f63bb2439adda95627f
Ruwanga Karunarathna <ruwanga@Ruwangas-MacBook-Air.local>
2025-10-30 17:22:44 +1100

    first commit added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("server is running");
+
+})
+
+// Start server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error("Failed to start server:", err.message);
+    process.exit(1);
+  }
+  console.log(`Server running at http://localhost:${PORT}`);
+});
